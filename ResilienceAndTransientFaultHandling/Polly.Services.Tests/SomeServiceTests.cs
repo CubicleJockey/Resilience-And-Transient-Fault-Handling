46791044cfbc76eb763c3f3b6e78922c521bb9a6
André Davis <CubicleJockey@users.noreply.github.com>
2017-07-20 15:10:01 -0700

    Getting setup

---
+++ ResilienceAndTransientFaultHandling/Polly.Services.Tests/SomeServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Polly.Services.Tests
+{
+    [TestFixture]
+    public class SomeServiceTests
+    {
+
+    }
+}
---
+++ ResilienceAndTransientFaultHandling/Polly.Services/SomeService.cs
@@ -0,0 +1,7 @@
+﻿namespace Polly.Services
+{
+    public class SomeService
+    {
+
+    }
+}
